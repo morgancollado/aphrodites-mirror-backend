23d54a792c3634a7bb9c24438dc7ebbd34871823
morgancollado <Morgan.collado@gmail.com>
2020-12-09 22:41:36 -0600

    sets up index and create controller actions

--- app/controllers/makeups_controller.rb
+++ app/controllers/makeups_controller.rb
@@ -1,2 +1,21 @@
 class MakeupsController < ApplicationController
+    def index 
+        makeups = Makeup.all
+        render json: makeups
+    end 
+
+    def create
+        makeup = Makeup.new(makeup_params)
+        if makeup.save 
+            render json: makeup, status: :accepted
+        else
+            render json: {errors: makup.errors.full_messages}, status: :unprocessible_entity
+        end 
+    end 
+
+    private 
+
+    def makeup_params
+        params.require(:makeup).permit(:product_name, :product_type, :skin_type, :brand, :shade)
+    end 
 end
